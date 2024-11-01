afbfc21ba2f06f09b8d8f920c3f6e3a40c6ba165
GarrettHiles <146273814+GarrettHiles@users.noreply.github.com>
2024-11-01 17:46:05 -0500

    PizzaMenu

---
+++ Menu/Menu/Data/MenuContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Menu.Models;
+
+
+namespace Menu.Data
+{
+    public class MenuContext : DbContext
+    {
+        public MenuContext(DbContextOptions<MenuContext> options) 
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<MenuIngredient>().HasKey(di => new
+            {
+
+                di.DishId,
+                di.IngredientId
+            });
+            modelBuilder.Entity<MenuIngredient>().HasOne(m => m.Menu).WithMany(di => di.MenuIngredients).HasForeignKey(m => m.DishId);
+            modelBuilder.Entity<MenuIngredient>().HasOne(i => i.Ingredient).WithMany(di => di.MenuIngredients).HasForeignKey(i => i.IngredientId);
+
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
---
+++ Menu/Menu/Models/Menu.cs
@@ -0,0 +1,12 @@
+﻿namespace Menu.Models
+{
+    public class Menu
+    {
+        public int MenuId { get; set; }
+        public string Name { get; set; }
+        public string ImageUrl { get; set; }
+        public double Price { get; set; }
+        
+        public List<MenuIngredient> MenuIngredients { get; set; }
+    }
+}
